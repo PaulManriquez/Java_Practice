5b483933f8bdc6371f17514801aba24b1c373ea1
PaulM <paulmanriquezengineer@gmail.com>
2025-02-14 23:44:38 -0600

    14_2_25

---
+++ SOLID_PRINCIPLES/Threads_practice1/src/HiloThread1.java
@@ -0,0 +1,33 @@
+
+public class HiloThread1 extends Thread {
+
+    public  HiloThread1(String name){
+        super(name);
+    }
+
+    public void run(){
+
+        for(int i=0; i<= 5; i++){
+            System.out.println("Thread : " + Thread.currentThread().getId() + i + getName());
+
+            try{
+                Thread.sleep(2000);//Sleeping 2seconds
+            }catch(InterruptedException e){
+                System.out.println("Error in thread : " + Thread.currentThread());
+            }
+
+        }//END FOR
+    }//END RUN METHOD
+
+
+    public static void main(String[] args){
+        //Objects declarations
+        HiloThread1 thread1 = new HiloThread1("FK 1:");
+        HiloThread1 thread2 = new HiloThread1("FK 2:");
+
+        thread1.start();
+        thread2.start();
+
+    }
+
+}
