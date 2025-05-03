098f7222b1b676b8e1b50130562df60a6b52c5dc
PaulM <paulmanriquezengineer@gmail.com>
2025-05-03 15:19:16 -0600

    How to load an image?, VacanciesController.java Utils.java application.properties formVacante.html

--- SpringBoot/jobOffers/src/main/java/net/itinajero/jobOffers/controller/VacanciesController.java
+++ SpringBoot/jobOffers/src/main/java/net/itinajero/jobOffers/controller/VacanciesController.java
@@ -70,6 +70,7 @@ public class VacanciesController {
 
     @Value("${spring.servlet.multipart.location}")
     private String uploadDir;
+    //application.properties: spring.servlet.multipart.location=${java.io.tmpdir}/myapp-uploads
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String saveCreateVacant(
@@ -109,64 +110,6 @@ public class VacanciesController {
         return "redirect:/vacancies/index";
     }
 
-
-//    @RequestMapping(value = "/save",method=RequestMethod.POST)
-//    public String saveCreateVacant(
-//            Vacancy vacant, /* <-- the attributes are filled automatically from the form*/
-//            BindingResult result,
-//            Model model,
-//            RedirectAttributes attributes,
-//            @RequestParam("archivoImagen") MultipartFile multiPart /*archivoImagen Comes from the form*/
-//    ){
-//        // Handling Error when for example in the field of salary ofered, an no number char has been introduced
-//        if (result.hasErrors()){
-//            for(ObjectError error: result.getAllErrors()){
-//                System.out.println("An error has happened: " + error.getDefaultMessage());
-//            }
-//            return "vacants/formVacante";
-//        }
-//
-//        //Upload image
-////        if(!multiPart.isEmpty()){
-////            String route = "src/main/resources/static/uploads/";
-////            String imageName = Utils.saveFile(multiPart,route);
-////            if(imageName!=null){
-////                vacant.setImage(imageName);
-////            }
-////        }
-//
-//
-//
-//
-//        // <-------------------------------
-//
-//        //Get the ID from the Form, and, set the Categoria object to the vacant
-//        Integer categoriaId = vacant.getCategoria().getId(); //Get the category ID of the vacant, according with the passed in the form
-//        Categoria fullCategoria = serviceCategories.buscarPorId(categoriaId); // search the id category by ID
-//        vacant.setCategoria(fullCategoria); // assign to the vacant the according object , is set to the vacant
-//
-//        //save and print the new vacant
-//        serviceVacancies.save(vacant);
-//        System.out.println(vacant.toString() );
-//        System.out.println(vacant.getCategoria().toString());
-//
-//        //Sent to the model the new list of vacants
-//        List<Vacancy> myListOfVacants = serviceVacancies.searchAllVacants();
-//        model.addAttribute("vacantsList",myListOfVacants);
-//
-//
-//        // <- Since we use a redirect in the return and redirect is a GET request
-//        // and here this controller is a POST request, the attribute do not exist in the
-//        // new GET request
-////        model.addAttribute("msg","New Register Saved");
-//// <-------------------------------------------------------------------------------------
-//
-//        //Redirect and Flash attributes
-//        attributes.addFlashAttribute("msg", "New Category Saved");
-//
-//        return "redirect:/vacancies/index";
-//        //return "vacants/listVacantes";
-//    }
 
     @InitBinder
     public void initBinder(WebDataBinder webDataBinder){
--- SpringBoot/jobOffers/src/main/java/util/Utils.java
+++ SpringBoot/jobOffers/src/main/java/util/Utils.java
@@ -11,18 +11,19 @@ import java.nio.file.Paths;
 public class Utils {
 
     public static String saveFile(MultipartFile multiPart, String route) {
-        String originalName = multiPart.getOriginalFilename();
-        File directory = new File(route);
+        String originalName = multiPart.getOriginalFilename(); // "photo.png"
+        File directory = new File(route);//Create the directory file object with the route to place
+        //the image and be able to create the dir and path to place it
 
         if (!directory.exists()) {
             directory.mkdirs(); // Create directory if it doesn't exist
         }
 
         try {
-            File imageFile = new File(directory, originalName);
-            System.out.println("Saving to: " + imageFile.getAbsolutePath());
-            multiPart.transferTo(imageFile);
-            return originalName;
+            File imageFile = new File(directory, originalName); // "Save the image in that directory" / Get the full path: /tmp/myapp-uploads/photo.png
+            System.out.println("Saving to: " + imageFile.getAbsolutePath());// Display the absolute path to place the image
+            multiPart.transferTo(imageFile); //  takes the uploaded file (in memory) and writes it to the file system
+            return originalName;//"photo.png"
 
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
