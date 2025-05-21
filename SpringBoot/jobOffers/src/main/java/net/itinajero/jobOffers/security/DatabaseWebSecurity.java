c2c2dd7cddbd226f48c5c6bb609c5484d5461607
Paul Manriquez <paulmanriquezengineer@gmail.com>
2025-05-20 20:48:44 -0600

    somework

--- SpringBoot/jobOffers/src/main/java/net/itinajero/jobOffers/security/DatabaseWebSecurity.java
+++ SpringBoot/jobOffers/src/main/java/net/itinajero/jobOffers/security/DatabaseWebSecurity.java
@@ -2,11 +2,15 @@ package net.itinajero.jobOffers.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -22,9 +26,9 @@ public class DatabaseWebSecurity {
     public UserDetailsManager usersCustom(DataSource dataSource){
         JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
 
-        users.setUsersByUsernameQuery("select username,password,estatus from Usuarios where username=?");
+        users.setUsersByUsernameQuery("select username,password,estatus from usuarios where username=?");//
 
-        
+                //Enable to this users permissions
                 users.setAuthoritiesByUsernameQuery(
                         "select u.username, p.perfil from usuario_perfil up " +
                                 "inner join usuarios u on u.id = up.idUsuario " +
@@ -33,4 +37,20 @@ public class DatabaseWebSecurity {
                         );
         return users;
     }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth //The users with this profiles have permissions
+                        .requestMatchers("/vacancies/**").hasAnyAuthority("SUPERVISOR", "ADMINISTRADOR")
+                        .requestMatchers("/categories/**").hasAnyAuthority("SUPERVISOR", "ADMINISTRADOR")
+                        .requestMatchers("/usuarios/**").hasAnyAuthority("ADMINISTRADOR")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults()); // or customize login
+
+        return http.build();
+    }
+
 }
