5ca21e5d61480b0babafee32fd000130ade1e56c
PaulM <paulmanriquezengineer@gmail.com>
2025-05-28 12:02:35 -0600

    inyect by attribute

---
+++ SPRING/inyectionByAttribute/src/test/java/com/inyeccion_by_attribute/inyectionByAttribute/InyectionByAttributeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inyeccion_by_attribute.inyectionByAttribute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InyectionByAttributeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
