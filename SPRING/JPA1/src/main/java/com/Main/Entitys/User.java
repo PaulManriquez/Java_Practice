2961ecfb8bee4d65697a5349016d53440c068242
Paul Manriquez <paulmanriquezengineer@gmail.com>
2025-06-02 15:15:52 -0600

    endWorkmodule8

---
+++ SPRING/JPA1/src/main/java/com/Main/Entitys/User.java
@@ -0,0 +1,69 @@
+package com.Main.Entitys;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity//NO A BEAN, makes it a JPA-managed object (a database table mapping).
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
+
+    @OneToOne// This column name | column referenced
+    @JoinColumn(name="profile_id",referencedColumnName = "id")
+    private Profile profile;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
